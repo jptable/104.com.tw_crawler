01f3f67cbdaebf29c70124936b693ba6034c43cf
jptable <68821771+jptable@users.noreply.github.com>
2022-12-14 10:43:25 +0800

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
@@ -5,75 +6,59 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import re
 
+def del_emoji(text):
+    emoji = re.compile('["\U00010000-\U0010ffff""●❒★▎■※↗↙•√！*▲◆【】"]', re.U)
+    return (re.sub(emoji, '', text))
+
+
+# 取得網址
+_url = input('請輸入待搜尋頁面網址：')
+
 # chromedriver 路徑
 service = Service(executable_path="D:/selenium/chromedriver")
 
 # get URL
 driver = webdriver.Chrome(service= service)
-driver.get("https://www.104.com.tw/jobs/search/?cat=2011000000&jobsource=2018indexpoc&ro=0")
+driver.get(_url)
 
-# 模擬滑鼠滾輪
-# 104 網頁 前17頁動態載入
-# 之後都手動載入
-for i in range(1,18):
+"""
+ 模擬滑鼠滾輪，
+ 104 網頁 前17頁動態載入，
+ 之後都手動載入
+"""
+
+
+for i in range(1,10):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(2)
 
-for i in range(19,283,2):
+for i in range(17,280,2):
     driver.find_element(By.XPATH, "/html/body/main/div[3]/div/div[2]/div[4]/div[%d]/button" % i).click()
     time.sleep(3)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-# 抓職缺名稱
 
-job_name = driver.find_elements(By.XPATH, '/html/body/main/div[3]/div/div[2]/div[4]/article/div[1]/h2/a')
-
-print("職位名稱共%d筆" % len(job_name))
-
-for i in range(0,len(job_name)):
-    print(job_name[i].text)
-    job_name[i] = job_name[i].text
+"""取得網頁資料"""
+jobs = driver.find_elements(By.XPATH, '/html/body/main/div[3]/div/div[2]/div[4]/article')
+for i in range(0, len(jobs)):
+    jobs[i] = jobs[i].text
+jobs = '||工作名稱:'.join(jobs)
 
 
-# 抓公司名稱
-company = driver.find_elements(By.XPATH, "/html/body/main/div[3]/div/div[2]/div[4]/article/div[1]/ul[1]/li[2]/a")
-print("公司名稱共%d筆" % len(company))
+"""資料清理"""
+jobs = del_emoji(jobs)
+jobs = re.sub('儲存\n應徵', '', jobs)
+jobs = re.sub('[0-9]+~[0-9]+人應徵', '', jobs)
+jobs = re.sub('大於30人應徵', '', jobs)
+jobs = re.sub('[-]', '', jobs)
 
-for i in range(0, len(company)):
-    print(company[i].text)
-    company[i] = company[i].text
+jobs = jobs.split('||')
+df = pd.DataFrame(jobs)
+print(jobs)
+print(df)
 
+filename = datetime.date.today()
 
-# 抓公司位置
-location = driver.find_elements(By.XPATH, "/html/body/main/div[3]/div/div[2]/div[4]/article/div[1]/ul[2]/li[1]")
-print("職缺位置共%d筆" % len(location))
-
-for i in range(0, len(location)):
-    print(location[i].text)
-    location[i] = location[i].text
-
-
-# 抓職缺簡介
-knowledge = driver.find_elements(By.XPATH, "/html/body/main/div[3]/div/div[2]/div[4]/article/div[1]/p")
-print("簡介共%d筆" % len(knowledge))
-
-for i in range(0, len(knowledge)):
-    print(knowledge[i].text)
-    knowledge[i] = knowledge[i].text
-
-
-# 存入excel
-
-filename = time.time() # 檔案名稱
-
-# zip multi list to a list
-df = pd.DataFrame(list(zip(job_name, company, location, knowledge)),
-                  columns=['jobs', 'company', 'location', 'knowledge'])
-
-print('\n', '\n', df)
-
-writer = pd.ExcelWriter("%d.xlsx" %filename, engine='xlsxwriter')
-df.to_excel(writer, sheet_name='worksheet', index=False)
-writer.save()
+df.to_csv(f'{filename}.csv')
 
 driver.quit()
